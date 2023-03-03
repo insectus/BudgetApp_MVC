88196c0365d6e8e09c556fbb0e8c9a463415dc4e
Damian Krząszcz <damiankrzaszcz@gmail.com>
2023-03-03 11:13:47 +0100

    Last commit of this project- project died

--- public/js/getCurrentDate.js
+++ public/js/getCurrentDate.js
@@ -1,3 +1,10 @@
-const dateInput = document.querySelector('#date');
-const currentDate = new Date();
-dateInput.value = currentData.toISOString().substr(0, 10);
+const element = document.getElementById('date-field');
+    setInterval(function () {
+        var MyDate = new Date();
+var MyDateString = new Date();
+MyDate.setDate(MyDate.getDate()+10);
+
+        element.innerText = ('0' + MyDate.getDate()).slice(-2) + '.'
+        + ('0' + (MyDate.getMonth()+1)).slice(-2) + '.'
+        + MyDate.getFullYear();
+      }, 1000);
